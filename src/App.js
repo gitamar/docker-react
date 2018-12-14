fe3d3353bc8abd69b761710a4545501f5ccf24ff
Amarnath Thillai <amar@192.168.1.5>
2018-12-14 10:44:17 +0530

    docker files added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            React Sucks.. hahahaha
           </p>
           <a
             className="App-link"
